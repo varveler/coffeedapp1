f04a9e625affb9184a111638ebf90ec05adaab08
varveler <evarval@gmail.com>
2015-04-18 15:46:45 -0500

    oush push

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from django.views.generic.base import TemplateView
+
+class LandingView(TemplateView):
+    template_name = "base/index.html"
+
+
 # Create your views here.
